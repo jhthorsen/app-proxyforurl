4e2e3771a4b1a3cc7d3c4b619b3eb15dd6c30bd4
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-11-13 18:02:22 +0100

    Released version 0.05
     - Fix dnsDomainIs() for domains containing dot "." #1

--- lib/App/proxyforurl.pm
+++ lib/App/proxyforurl.pm
@@ -2,7 +2,7 @@ package App::proxyforurl;
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 1;
 
@@ -14,7 +14,7 @@ App::proxyforurl - An online proxy PAC file parser
 
 =head1 VERSION
 
-0.04
+0.05
 
 =head1 DESCRIPTION
 
