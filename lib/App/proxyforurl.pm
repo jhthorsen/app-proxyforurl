782ad6c353c7d441469ae82478a57be6fd4af14e
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-12-11 09:55:27 +0100

    Released version 0.06
     - Fix dnsDomainIs() should not use a regex matcher #2

--- lib/App/proxyforurl.pm
+++ lib/App/proxyforurl.pm
@@ -2,7 +2,7 @@ package App::proxyforurl;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 1;
 
@@ -14,7 +14,7 @@ App::proxyforurl - An online proxy PAC file parser
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 DESCRIPTION
 
